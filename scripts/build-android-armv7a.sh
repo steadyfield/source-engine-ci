ce5599bc80974aa579267626b0cc55c1cec44e41
vl <vladchudov636@gmail.com>
2024-08-18 21:56:45 +0500

    again fix build scripts

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -6,8 +6,8 @@ export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -o /dev/null
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
-./waf configure -T release --build-game=hl2sb --prefix=srceng-mod-launcher/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --32bits --enable-opus --togles --disable-warns &&
-./waf install --target=client,server --strip
+./waf configure -T release --build-game=smodredux --prefix=srceng-mod-launcher/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --32bits --enable-opus --togles --disable-warns &&
+./waf install --target=client,server,stdshader_dx9 --strip
 
 if [ -e "srceng-mod-launcher/android/lib/armeabi-v7a/README.md" ]; then
 	rm srceng-mod-launcher/android/lib/armeabi-v7a/README.md

40d006ab200554d9b633d798fb59cbdb6209a22e
vl <vladchudov636@gmail.com>
2024-09-19 14:12:50 +0500

    Build Engine Binaries with episodic game

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -6,7 +6,7 @@ export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -o /dev/null
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
-./waf configure -T release --build-game=lostcoast --prefix=srceng-mod-launcher/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --32bits --enable-opus --togles --disable-warns &&
+./waf configure -T release --build-game=episodic --prefix=srceng-mod-launcher/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --32bits --enable-opus --togles --disable-warns &&
 ./waf install --target=client,server --strip
 
 if [ -e "srceng-mod-launcher/android/lib/armeabi-v7a/README.md" ]; then
--- scripts/build-ubuntu-amd64.sh
+++ scripts/build-ubuntu-amd64.sh
@@ -4,5 +4,5 @@ git submodule init && git submodule update
 sudo apt-get update
 sudo apt-get install -f -y libopenal-dev g++-multilib gcc-multilib libpng-dev libjpeg-dev libfreetype6-dev libfontconfig1-dev libcurl4-gnutls-dev libsdl2-dev zlib1g-dev libbz2-dev libedit-dev
 
-./waf configure -T release --build-game=lostcoast --prefix=bin --disable-warns $* &&
+./waf configure -T release --build-game=episodic --prefix=bin --disable-warns $* &&
 ./waf install --strip

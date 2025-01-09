47d498007ca6c7413648c0d788cf0fc7bfbe1e0c
vl <vladchudov636@gmail.com>
2025-01-10 00:55:48 +0500

    GUNMOD 0.6 Fix

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -7,7 +7,7 @@ wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
 ./waf configure -T release --build-game=gunmod --prefix=srceng-mod-launcher/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --32bits --enable-opus --togles --disable-warns &&
-./waf install --target=client,server,gameui --strip
+./waf install --target=client,server,GameUI --strip
 
 if [ -e "srceng-mod-launcher/android/lib/armeabi-v7a/README.md" ]; then
 	rm srceng-mod-launcher/android/lib/armeabi-v7a/README.md
